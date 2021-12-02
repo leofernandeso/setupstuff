7f26fe9c541ec0caebe9e6be478d542ca3287e05
leofernandeso <leonardo.fernandes.oliv@gmail.com>
2021-12-02 13:14:57 +0100

    Add sound control hotkey to dwm build

--- dwm_build/config.h
+++ dwm_build/config.h
@@ -62,6 +62,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "terminator", NULL };
 static const char *chromecmd[]  = { "google-chrome", NULL };
+static const char *soundcontrolcmd[]  = { "pavucontrol", NULL };
 
 #include "shiftview.c"
 static Key keys[] = {
@@ -69,6 +70,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_n,      spawn,          {.v = chromecmd } },
+	{ MODKEY|ShiftMask,             XK_s,      spawn,          {.v = soundcontrolcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
