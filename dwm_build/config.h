6d35aa8776057aa482151818a7db853488d71ccd
leofernandeso <leonardo.fernandes.oliv@gmail.com>
2022-05-14 17:28:17 +0200

    Add volume control commands to dwm

--- dwm_build/config.h
+++ dwm_build/config.h
@@ -1,4 +1,6 @@
 /* See LICENSE file for copyright and license details. */
+// X11 XF86 header for custom keys (like volume control)
+#include <X11/XF86keysym.h>
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
@@ -58,21 +60,27 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "gnome-terminal", NULL };
-static const char *chromecmd[]  = { "google-chrome", NULL };
+static const char *termcmd[]  = { "st", NULL };
+static const char *firefoxcmd[]  = { "firefox", NULL };
 static const char *soundcontrolcmd[]  = { "pavucontrol", NULL };
 static const char *bluemanmanagercmd[]  = { "blueman-manager", NULL };
 static const char *slockcmd[]  = { "slock", NULL };
+static const char *mutecmd[] = { "pactl", "set-sink-mute", "0", "toggle", NULL };
+static const char *volupcmd[] = { "pactl", "set-sink-volume", "0", "+5%", NULL };
+static const char *voldowncmd[] = { "pactl", "set-sink-volume", "0", "-5%", NULL };
 
 #include "shiftview.c"
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_n,      spawn,          {.v = chromecmd } },
+	{ MODKEY,                       XK_n,      spawn,          {.v = firefoxcmd } },
 	{ MODKEY|ShiftMask,             XK_s,      spawn,          {.v = soundcontrolcmd } },
 	{ MODKEY|ShiftMask,             XK_b,      spawn,          {.v = bluemanmanagercmd } },
 	{ MODKEY|ShiftMask,             XK_l,      spawn,          {.v = slockcmd } },
+    { 0,              XF86XK_AudioMute,        spawn,          {.v = mutecmd } },
+    { 0,              XF86XK_AudioLowerVolume, spawn,          {.v = voldowncmd } },
+    { 0,              XF86XK_AudioRaiseVolume, spawn,          {.v = volupcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
--- statusbar/autostart.sh
+++ statusbar/autostart.sh
@@ -1,4 +1,4 @@
 while true; do
     xsetroot -name "$(cat /sys/class/power_supply/BAT0/capacity)% | $(date +"%F %R")"
     sleep 1m    # Update status bar every minute
-done &
+done
